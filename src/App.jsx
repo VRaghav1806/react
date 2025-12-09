bc4bb28e2be25307f0daf07065597fa5b411dea4
raghav <raghavvenkat18@gmail.com>
2025-12-09 12:53:43 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,20 @@
-import React from "react";
-import lap from "./assets/laptop.png";
-import mob from "./assets/mobile.png";
-import "./App.css";
+import { useEffect } from 'react';
+import React, { useState } from 'react'
 
-function App() {
-  const productlist = [
-    {
-      name: "Laptop",
-      discription: "This is a laptop",
-      price: 2000,
-      image: lap,
-    },
-    {
-      name: "Mobile",
-      discription: "This is a mobile",
-      price: 1000,
-      image: mob,
-    },
-  ];
-
+export default function App() {
+  const [name, setName] = useState("");
+  const handleSave=()=>{
+    const user={
+      name:name,
+      role:"Student"
+    };
+    localStorage.setItem("Name",JSON.stringify(user));
+  }
   return (
-    <div className="app">
-      <div className="product-page">
-        <h1 className="title">PRODUCT LIST</h1>
-
-        <div className="product-list">
-          {productlist.map((item, index) => (
-            <article className="product-card" key={index}>
-              <div className="product-card-image">
-                <img src={item.image} alt={item.name} />
-              </div>
-
-              <div className="product-card-body">
-                <h2 className="product-name">{item.name}</h2>
-                <p className="product-description">{item.discription}</p>
-
-                <div className="product-card-footer">
-                  <span className="product-price">₹{item.price}</span>
-                  <button className="buy-button">Buy Now</button>
-                </div>
-              </div>
-            </article>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    <>
+      <input placeholder="Name" value={name} onChange={(e)=>setName(e.target.value)}/>
+      <button onClick={handleSave}>Save</button>
+      <p>{localStorage.getItem("Name")}</p>
+    </>
+  )
 }
-
-export default App;
